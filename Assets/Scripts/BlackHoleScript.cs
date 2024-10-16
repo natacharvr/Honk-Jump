8d99cbbc3f303c6572b344173cd7dff5372ec3ef
Natacha Rivière <riviere.natachamarie@gmail.com>
2024-10-16 18:04:53 +0200

    tried adding black holes

---
+++ Assets/Scripts/BlackHoleScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleScript : MonoBehaviour
+{
+    public GameObject gameManager;
+    public GameObject player;
+    // Start is called before the first frame update
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GetComponent<GameManagerScript>().GameOver();
+        player.SetActive(false);
+    }
+}
